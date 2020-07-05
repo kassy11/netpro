21906f2403cef1ebf03a09dda04f2cab9d3ba373
kassy11 <kotaro11kassy@gmail.com>
2020-07-05 14:05:03 +0900

    finish hw2

--- hw2/main.c
+++ hw2/main.c
@@ -1,12 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
-
 #include "mynet.h"
 
 #define S_BUFSIZE 100 /* 送信用バッファサイズ */
@@ -15,85 +6,110 @@
 void list(char*);
 void type(char*, char*);
 
-int main(int argc, char *argv[]) {
-    int port = atoi(argv[2]);
+int main(int argc, char* argv[])
+{
     int sock_listen, sock_accepted;
     char s_buf[S_BUFSIZE], r_buf[R_BUFSIZE];
-    char prompt[2] = ">";
+    char prompt[] = ">";
     int prompt_size = 2;
     int strsize;
 
-    // mynetを利用してサーバに接続
-    sock_listen = init_tcpserver(port, 5);
+    if( argc != 2 ){
+        fprintf(stderr,"Usage: %s Port_number\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    int port_num = atoi(argv[1]);
 
-    while(1){
+    sock_listen = init_tcpserver(port_num, 5);
+
+    while (1)
+    {
         sock_accepted = accept(sock_listen, NULL, NULL);
 
-        while(1){ // exitで抜けたときにもう一度受付状態に戻れるように２重ループにする
-            fprintf(stderr,">");
-            fflush(stderr);
-            // 文字入力
-            do{
-                if((strsize=recv(sock_accepted, r_buf, R_BUFSIZE, 0)) == -1){
+        while (1) // コマンド入力後、新たなクライアントからの接続待ちにうつれるように２重ループ
+        {
+            // プロンプトの>をクライアントに送信する
+            if (send(sock_accepted, prompt, prompt_size, 0) == -1)
+            {
+                exit_errmesg("send()");
+            }
+
+            /* サーバから文字列を受信する */
+            do
+            {
+                if ((strsize = recv(sock_accepted, r_buf, R_BUFSIZE, 0)) == -1)
+                {
                     exit_errmesg("recv()");
                 }
-//                if (send(sock_accepted, s_buf, strsize, 0) == -1)
-//                {
-//                    exit_errmesg("send()");
-//                }
-            }while(r_buf[strsize-1] != '\n');
-            r_buf[strsize] = '\0';
+                r_buf[strsize] = '\0';
+            } while (r_buf[strsize - 1] != '\n');
 
             fflush(stdout);
 
-            if(strstr(r_buf, "exit") != NULL){
-                printf("Connection closed by foreign host.\n");
+            if (strstr(r_buf, "exit") != NULL)
+            {
                 break;
-            }else if(strstr(r_buf, "list") != NULL){
+                // 内ループから抜ける
+            }
+            else if (strstr(r_buf, "list") != NULL)
+            {
                 list(s_buf);
-            }else if(strstr(r_buf, "type") != NULL){
+            }
+            else if (strstr(r_buf, "type") != NULL)
+            {
                 type(r_buf, s_buf);
-            }else{
-                exit_errmesg("unknown command!!");
+            }
+            else
+            {
+                exit_errmesg("unknown command!");
+            }
+
+            // listとtypeでs_bufをセットしてsend()する
+            strsize = strlen(s_buf);
+            if (send(sock_accepted, s_buf, strsize, 0) == -1)
+            {
+                exit_errmesg("send()");
             }
         }
-        // 次の受け入れのために受け入れソケット閉じる
-        close(sock_accepted);
+        close(sock_accepted); /* ソケットを閉じる */
     }
 
-    close(sock_listen);
+    close(sock_listen); /* ソケットを閉じる */
     exit(EXIT_SUCCESS);
-    return 0;
 }
 
-void list(char *s_buf){
-    strcpy(s_buf, "");
+void list(char *s_buf)
+{
+    strcpy(s_buf, ""); // s_bufを初期化
     char buff[1024];
-    FILE *fp = popen("ls ./", "r");
-    while (fgets(buff, sizeof(buff), fp))
+    FILE *fp = popen("ls ./", "r"); // popenでunixコマンドの出力を読み込む
+    while (!feof(fp))
     {
-        strcat(s_buf, buff);
+        fgets(buff, sizeof(buff), fp); // fpの出力を読み込む
+        strcat(s_buf, buff); // s_bufに連結していく
     }
     pclose(fp);
 }
 
-
 void type(char *r_buf, char *s_buf)
 {
-    strcpy(s_buf, "");
+    strcpy(s_buf, ""); // s_bufを初期化
     char buff[1024];
     char *tp;
     char cmd[R_BUFSIZE];
-    strtok(r_buf, " ");
+    strtok(r_buf, " "); // r_bufをスペースで区切る
     tp = strtok(NULL, "\r");
     if (tp == NULL)
         strcpy(s_buf, "error!\n");
 
-    sprintf(cmd, "cat ~/work/%s", tp);
+    sprintf(cmd, "cat ./%s", tp);
     FILE *fp = popen(cmd, "r");
     while (fgets(buff, sizeof(buff), fp))
     {
         strcat(s_buf, buff);
     }
+    strcat(s_buf, "\n");
     pclose(fp);
 }
+
+
